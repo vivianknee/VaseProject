84a8de250de97b21ca089474a27603a4b1404411
vivianknee <vivianknee@users.noreply.github.com>
2022-10-31 22:44:55 +0000

    deploy: 79a6b1e3c2e3168340257d02bbae86f2391d612f

--- checkscript.js
+++ checkscript.js
@@ -1,11 +1,16 @@
-function isChecked(){
-    if(document.getElementById("my-checkbox").
-    checked){
-        document.getElementById("message").
-        textContent = "Checked";
-    }
-    else{
-        document.getElementById("message").
-        textContent = "Not Checked";
-    }
+function checkboxes() {
+    var box = document.getElementById("box");
+     
+    // creating checkbox element
+    var checkbox = document.createElement('input');
+     
+    // Assigning the attributes
+    // to created checkbox
+    checkbox.type = "checkbox";
+    checkbox.name = "name";
+    checkbox.value = "value";
+    checkbox.id = "id";
+     
+    // appending the checkbox to div
+    box.appendChild(checkbox);
 }
